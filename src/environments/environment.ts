e82fa025b15a7a54ba3ed0112faf4897aead2c8d
Rishu katiayr <rishukatiyar001@gmail.com>
2025-03-03 12:10:30 +0530

    Include environment.ts in deployment

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  mainColor: '#3867d6',
+  url: 'http://localhost:4200',
+  
+  //umamiWebsiteId use for web analytics to use make changes in app.ts //
+  // umamiWebsiteId: '', 
+};
